5e55bcb57746cbec585143de30afc9578d1e9202
Tcodingg <princetlive@yahoo.com>
2022-02-10 12:40:36 -0500

    move the context folder to root folder

--- context/index.js
+++ context/index.js
@@ -1,7 +1,6 @@
 import { createContext, useState } from "react";
 
 export const Context = createContext();
-
 export const ContextProvider = (props) => {
    const [userName, setUserName] = useState("");
    const [secrets, setSecret] = useState("");
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,26 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import { Context } from "../context";
+import { useRouter } from "next/router";
+import { useContext } from "react";
 
 export default function Auth() {
+   const { userName, setUserName, secrets, setSecret } = useContext(Context);
    return (
-      <div>
-         <p>auth</p>
+      <div className="background">
+         <div className="auth-container">
+            <form className="auth-form">
+               <div className="auth-title">Chat App</div>
+               <div className="input-container">
+                  <input
+                     type="text"
+                     placeholder="Email"
+                     className="text-input"
+                  />
+               </div>
+            </form>
+         </div>
       </div>
    );
 }
